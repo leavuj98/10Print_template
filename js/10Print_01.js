871006f3153f4422eee33682ff7feccf7b641a84
leavuj98 <169165203+leavuj98@users.noreply.github.com>
2024-06-30 22:18:14 +0200

    Update 10Print_01.js
    added other pattern

--- js/10Print_01.js
+++ js/10Print_01.js
@@ -1,38 +1,82 @@
-let x = 0;
-let y = 0;
-let size = 60;
+let circles = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  let i = color('rgb(235,29,226)');
-  background(i);
+  
+  // Nessun riempimento per i cerchi
+  noFill(); 
+  
+  // Spessore del contorno delle forme
+  strokeWeight(2); 
+  
+  for (let i = 0; i < 100; i++) {
+    
+    let x = random(width);
+    let y = random(height);
+    let radius = random(20, 50);
+    let opacity = random(50, 200);
+    let color1 = color(random(50, 200), random(50, 200), random(50, 200), opacity);
+    let color2 = color(random(50, 200), random(50, 200), random(50, 200), opacity);
+    
+    // Gradients per ogni cerchio
+    let gradient = [color1, color2]; 
+    let xSpeed = random(-1, 1);
+    let ySpeed = random(-1, 1);
+
+    circles.push(new Circle(x, y, radius, gradient,                             xSpeed, ySpeed));
+  }
 }
 
+
 function draw() {
-  let c = color("rgb(154,86,250)");
-  let a = color("red");
-    let b = color("rgb(236,176,228)");
-  strokeWeight(2);
-  let r = random(3);
+  
+  // Sfondo nero
+  background(0); 
+  for (let circle of circles) {
+    circle.move();
+    circle.display();
+  }
+}
 
-  if (r < 1) {
-    fill(a);
-    stroke (255);
-    ellipse(x, y, size);
-    //line(x, y, x+size, y+size);
-  } else if(r < 2){
-    //line(x, y+size, x+size, y);
-    fill(c);
-    rect(x, y, size);
-  } else if(r < 3) {
-    fill (b)
-    triangle (x,y,x + size ,y - size, x + size,y + size);
-  } 
-    
 
-  x += size;
-  if (x > width) {
-    x = 0;
-    y = y + size;
+class Circle {
+  constructor(x, y, radius, gradient, xSpeed, ySpeed) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.gradient = gradient;
+    this.rotationSpeed = random(-0.02, 0.02);
+    this.rotationAngle = 0;
+    this.xSpeed = xSpeed;
+    this.ySpeed = ySpeed;
+  }
+
+  move() {
+    this.x += this.xSpeed;
+    this.y += this.ySpeed;
+
+    // Controlla i bordi e inverti la direzione se necessario
+    if (this.x < 0 || this.x > width) {
+      this.xSpeed *= -1;
+    }
+    if (this.y < 0 || this.y > height) {
+      this.ySpeed *= -1;
+    }
+  }
+
+  
+  display() {
+    this.rotationAngle += this.rotationSpeed;
+    let d = this.radius * 2;
+    push();
+    translate(this.x, this.y);
+    rotate(this.rotationAngle);
+    for (let i = 0; i < d; i += 4) {
+      let inter = map(i, 0, d, 0, 1);
+      let c = lerpColor(this.gradient[0], this.gradient[1], inter);
+      stroke(c);
+      ellipse(0, 0, i);
+    }
+    pop();
   }
 }
